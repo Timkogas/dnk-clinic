830485d929880aa3e99c0bb8bc66591256792d06
timkogas <timkogas@gmail.com>
2024-01-12 15:05:37 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,32 +5,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import bridge from '@vkontakte/vk-bridge'
-import { HashRouter as BrowserRouter } from 'react-router-dom';
-import { Navigate } from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 
 bridge.send("VKWebAppInit");
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const shouldRedirectToHome = () => {
-  const queryParams = new URLSearchParams(window.location.search);
-  // Clear all query parameters
-  window.history.replaceState({}, document.title, window.location.pathname);
-  
-  // Check for the presence of hash fragment and redirect to home
-  const shouldRedirect = window.location.hash.includes('#vk.com/app51759006');
-  if (shouldRedirect) {
-    window.location.hash = ''; // Clear the hash fragment
-  }
-
-  return shouldRedirect;
-};
-
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL === '.' ? '/' : process.env.PUBLIC_URL}>
     <Provider store={store}>
-    {shouldRedirectToHome() && <Navigate to="/" replace />}
       <App />
     </Provider>
   </BrowserRouter>
